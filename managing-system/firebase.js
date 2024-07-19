296d97c5cf2eda109346761c540ea2b64c4581d0
David <david.junio@gmail.com>
2024-07-19 10:42:41 -0300

    just a security change

--- managing-system/firebase.js
+++ managing-system/firebase.js
@@ -4,15 +4,7 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, doc, setDoc } from '@firebase/firestore';
 
-var firebaseConfig = {
-    apiKey: "AIzaSyB2ERsfXYYFKl9naYxCtnjBobIsq5Inf78",
-    authDomain: "middleware-extendify.firebaseapp.com",
-    projectId: "middleware-extendify",
-    storageBucket: "middleware-extendify.appspot.com",
-    messagingSenderId: "567977011065",
-    appId: "1:567977011065:web:040acb41ac41d7f57531a8",
-    measurementId: "G-EG7FS9T7PT"
-};
+
 // Initialize Firebase
 // firebase.initializeApp(firebaseConfig);
 
